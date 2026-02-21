686d2061417811cc1f5786ccf2ef43a812ad08d8
Link4Electronics <link4electronics@gmail.com>
2026-02-21 04:50:12 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,7 @@ echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     libdecor      \
     libjpeg-turbo \
-    lua           \
+    lua53           \
     sdl2          \
     openal        \
     premake
@@ -31,7 +31,7 @@ echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 cd ./REDRIVER2/src_rebuild
-sed -i "s/'uses'/'rd2_uses'/g" premake_modules/usage/usage.lua
+#sed -i "s/'uses'/'rd2_uses'/g" premake_modules/usage/usage.lua
 premake5 gmake
 make config=release_x64 -j$(nproc)
 mv -v REDRIVER2 ../AppDir/bin
