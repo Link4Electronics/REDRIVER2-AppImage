b794db2d7b68033cdf21d1e20b823939d150403c
Link4Electronics <link4electronics@gmail.com>
2026-02-23 18:21:45 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -45,12 +45,12 @@ sed -i 's/libdirs {/libdirs {\n\t\t"PsyCross\/bin\/Release",\n\t\t"PsyCross\/bin
 if [ "$ARCH" = "aarch64" ]; then
     sed -i 's/platforms { "x86", "x64" }/platforms { "x86", "x64", "arm64" }/g' premake5.lua
     sed -i '/filter "system:Linux"/a \ \ \ \ \ \ \ \ buildoptions { "-fpack-struct=4", "-fpermissive", "-flax-vector-conversions", "-include cstdint" }' premake5.lua
-    sed -i '1i #include <sys/types.h>' PsyCross/include/psx/types.h
-    sed -i 's/typedef unsigned long   u_long;/#define u_long unsigned int/g' PsyCross/include/psx/types.h
-    sed -i 's/typedef unsigned long   ulong;/#define ulong unsigned int/g' PsyCross/include/psx/types.h
-    sed -i 's/typedef long            long32;/typedef int             long32;/g' PsyCross/include/psx/types.h
+    sed -i '1i #include <stdint.h>' PsyCross/include/psx/types.h
+    sed -i 's/typedef unsigned long   u_long;/#define u_long uint32_t/g' PsyCross/include/psx/types.h
+    sed -i 's/typedef unsigned long   ulong;/#define ulong uint32_t/g' PsyCross/include/psx/types.h
+    sed -i 's/typedef long            long32;/#define long32 int32_t/g' PsyCross/include/psx/types.h
     find PsyCross/include/psx/ -name "*.h" -exec sed -i 's/\blong\b/int/g' {} +
-    find PsyCross/include/psx/ -name "*.h" -exec sed -i 's/void\s*\*.*tag;/unsigned int tag;/g' {} +
+    find PsyCross/include/psx/ -name "*.h" -exec sed -i 's/void\s*\*.*tag;/uint32_t tag;/g' {} +
     find PsyCross/include/psx/ -name "*.h" -exec sed -i 's/#define P_LEN.*/#define P_LEN (0)/g' {} +
     sed -i 's/(int)vsync_callback/(uintptr_t)vsync_callback/g' PsyCross/src/psx/LIBETC.C
 
