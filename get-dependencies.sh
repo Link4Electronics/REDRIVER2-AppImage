7e5144d6c0187dbd2486fec2777f3152e39140c0
Link4Electronics <link4electronics@gmail.com>
2026-02-23 00:15:13 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -44,8 +44,10 @@ sed -i 's/links {/links {\n\t\t"PsyCross",\n\t\t"m",/g' premake5.lua
 sed -i 's/libdirs {/libdirs {\n\t\t"PsyCross\/bin\/Release",\n\t\t"PsyCross\/bin\/Debug",/g' premake5.lua
 if [ "$ARCH" = "aarch64" ]; then
     sed -i 's/platforms { "x86", "x64" }/platforms { "x86", "x64", "arm64" }/g' premake5.lua
-    sed -i '/filter "system:Linux"/a \ \ \ \ \ \ \ \ buildoptions { "-fpack-struct=4", "-fpermissive", "-flax-vector-conversions", "-D_SYS_TYPES_H", "-Du_long=unsigned int", "-Dulong=unsigned int" }' premake5.lua
+    sed -i '/filter "system:Linux"/a \ \ \ \ \ \ \ \ buildoptions { "-fpack-struct=4", "-fpermissive", "-flax-vector-conversions" }' premake5.lua
     sed -i 's/typedef long            long32;/typedef int             long32;/g' PsyCross/include/psx/types.h
+    sed -i 's/typedef unsigned long   u_long;/typedef unsigned int    u_long;/g' PsyCross/include/psx/types.h
+    sed -i 's/typedef unsigned long   ulong;/typedef unsigned int     ulong;/g' PsyCross/include/psx/types.h
     premake5 gmake
     cd build
     make config=release_arm64 -j$(nproc)
